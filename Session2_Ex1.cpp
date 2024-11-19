c166cd68fca456119006d60fdbfa970494527fe6
vvd-110106 <doanvuvan411@gmail.com>
2024-11-19 22:14:40 +0700

    Session2_Ex1

---
+++ Session2_Ex1.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	// khai bao va khoi tao cua bien int ( so nguyen )
+	int a=10, b=-5, c=0;
+	printf("a= %d ;  b= %d ; c= %d \n", a,b,c);
+	// khai bao va khoi tao bien float ( so thuc )
+	float pi = 3.14;
+	printf("pi= %.2f \n",pi);
+	// khai bao va khoi tao cua bien double 
+	double d = 3.14;
+	printf("d= %.5lf\n", d);
+	// khai bao va khoi bao bien cua char
+	char f= '#';
+	printf("f= %c",f);
+	return 0;
+}
